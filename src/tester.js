29e4960e782fc14435aa456fce1d69078167165e
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2020-09-09 21:07:09 +0300

    feat: did 2 modules: carbon-dating, count-cats

---
+++ src/tester.js
@@ -0,0 +1,7 @@
+let carbon = require('./carbon-dating.js')
+
+console.log(carbon('hello'))
+console.log(carbon('11.3231.3213124'))
+console.log(carbon('0'))
+console.log(carbon('8.738732722522064'))
+console.log(carbon(''))
