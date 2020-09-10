1876e2c90c757057b5f271fcc59012e848aad117
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2020-09-10 22:46:30 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options === undefined)
+        return ''
+    let line = ''
+    let separator = options.separator === undefined ? '+' : options.separator
+    let addition = options.addition === undefined ? '' : options.addition
+    let additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator
+    let repeatTimes = options.repeatTimes === undefined ? 0 : options.repeatTimes
+    let additionRepeatTimes = options.additionRepeatTimes === undefined ? 0 : options.additionRepeatTimes
+    if (repeatTimes == 0 && additionRepeatTimes==0) return str+addition
+    for (let i = 0; i < repeatTimes; i++) {
+        line += str
+        for (let j = 0; j < additionRepeatTimes - 1; j++) {
+            line += addition
+            line += additionSeparator
+        }
+        line += addition
+        if (i < repeatTimes - 1)
+            line += separator
+    }
+    return line
 };
-  
--- src/tester.js
+++ src/tester.js
@@ -1,6 +1,2 @@
-let chainMaker = require('./simple-chain.js')
-let a = [1,2,3]
-a.reverse()
-console.log(a)
-console.log(chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain())
-
+let repeater = require('./extended-repeater.js')
+console.log(repeater('TESTstr', { repeatTimes: undefined, separator: 'ds', addition: 'ADD!', additionRepeatTimes: undefined, additionSeparator: ')))000' }));
