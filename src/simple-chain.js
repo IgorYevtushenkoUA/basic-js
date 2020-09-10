9f0a76c87552418d87abf5e30c07df65adbea1cd
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2020-09-10 20:23:39 +0300

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+    chainDate: [],
+    deleteChain() {
+        this.chainDate = []
+    },
+    getLength() {
+        return this.chainDate.length
+    },
+    addLink(value) {
+        if (value === undefined)
+            this.chainDate.push('( )')
+        else
+            this.chainDate.push(`( ${value} )`)
+        return this
+    },
+    removeLink(position) {
+        if (!(Number.isInteger(position) &&
+            Number.parseInt(position) < this.chainDate.length)) {
+            this.deleteChain()
+            throw new Error
+        }
+        this.chainDate.slice(position-1,1)
+        return this
+    },
+    reverseChain() {
+        let temp = []
+        for (let i = (this.chainDate.length) -1 ; i >=0 ; i--)
+            temp.push(this.chainDate[i])
+        this.chainDate = temp
+        return this
+    },
+    finishChain() {
+        console.log(this.chainDate)
+        console.log(this.chainDate.length)
+        let temp = this.chainDate.join('~~')
+        this.deleteChain()
+        return temp
+    }
+
+}
 
 module.exports = chainMaker;
--- src/tester.js
+++ src/tester.js
@@ -1,7 +1,6 @@
-let arr = require('./transform-array.js')
-
-console.log(arr(['--discard-prev', 1, 2, 3]));
-console.log(arr([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]));
-// console.log(arr([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]));
-// console.log(arr([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]));
+let chainMaker = require('./simple-chain.js')
+let a = [1,2,3]
+a.reverse()
+console.log(a)
+console.log(chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain())
 
