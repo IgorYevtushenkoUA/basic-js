1d1109ddd63b435bf7cff68aa8dc8425d60529bc
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2020-09-10 12:22:22 +0300

    feat: hanoi-tower

--- src/tester.js
+++ src/tester.js
@@ -1,16 +1,10 @@
-let weather = require('./what-season.js')
+let hanoi = require('./hanoi-tower.js')
 
-
-const date1 = new Date('December 17, 1995 03:24:00');
-const date = new Date(98, 1);
-console.log(weather(new Date('Thu Sep 10 2020 11:23:49 GMT+0300 (GMT+03:00)')))
-console.log(weather(date1))
-console.log(weather(date))
-console.log(weather(date1))
-console.log(weather('foo'));;
-console.log(weather({John: 'Smith'}));;
-console.log(weather(20192701));;
-console.log(weather([2019, '27', 0 + '1']));;
-console.log(weather(() => new Date()));;
-
+console.log(hanoi(5, 4074));
+console.log(hanoi(38, 4594));
+console.log(hanoi(34, 4005));
+console.log(hanoi(19, 4770));
+console.log(hanoi(23, 4344));
+console.log(hanoi(9, 4347));
+console.log(hanoi(44, 4685));
 
